19b28ab8942f126490976bef18bf6e192441d0cd
Sharee Thompson <sharee.thompson@vml.com>
2026-04-30 13:52:03 -0500

    Add visual regression pilot baselines

--- utils/paths.ts
+++ utils/paths.ts
@@ -1,9 +1,9 @@
 const paths = [
-	"/",
-	"/lawn-care-101",
-	"/customer-support",
-	"/products-and-services",
-	"/why-choose-trugreen/testimonials-and-ratings"
+  "/",
+  "/lawn-care-101",
+  "/customer-support",
+  // "/products-and-services",
+  // "/why-choose-trugreen/testimonials-and-ratings"
 ];
 
 export default paths;
