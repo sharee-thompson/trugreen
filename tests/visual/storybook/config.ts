a9b4fc092b43702c4989282198316f240323a748
Jessica <jessica.zager@vml.com>
2026-05-11 16:16:33 -0500

    Set up list of stories that would require a click

--- tests/visual/storybook/config.ts
+++ tests/visual/storybook/config.ts
@@ -1,4 +1,5 @@
 import path from "path";
+import { Page } from "@playwright/test";
 
 const STORYBOOK_BASE = "https://tru-g-2025.netlify.app";
 
@@ -12,3 +13,32 @@ export const STORYBOOK_CONFIG = {
 
   renderTimeout: 5000,
 };
+
+//List of stories that require a click to reveal story under test
+type StoryInteraction = (page: Page) => Promise<void>;
+
+export const STORY_INTERACTIONS: Record<string, StoryInteraction> = {
+  //Only story as of May 11, default modals, still under construction. JZ
+  "components-v2-modals-rightmodal--default": async (page) => {
+    await page.locator(".btn primary-btn").first().click();
+  },
+  /*"components-v2-modal--default": async (page) => {
+    await page.getByRole("button", { name: "Open" }).click();
+  },
+  "components-v2-dropdown--closed": async (page) => {
+    await page.getByRole("combobox").click();
+  },*/
+};
+
+/*
+Stories with hover state validation
+- a class="cta_cta__klq2F"
+- button class="cta_cta__klq2F"
+- label class="addOnCard_cta__bm9aX"
+- label class="paymentCard_radioLabel__iaSYg"
+- label class="planCard_cta__q3fq_"
+- button class="ribbon_btn-custom-quote__pBMK7 cta_cta__klq2F"
+
+Cursor change?
+- a id="play_store_link" or "app_store_link"
+*/
