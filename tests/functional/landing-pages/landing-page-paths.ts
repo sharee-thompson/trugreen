09d84580ae973f338de485b8f4397c2c60863a7e
Jessica <jessica.zager@vml.com>
2026-05-13 12:46:54 -0500

    I think this approach is cleaner

--- tests/functional/landing-pages/landing-page-paths.ts
+++ tests/functional/landing-pages/landing-page-paths.ts
@@ -1,5 +1,16 @@
 import { getBaseUrl } from "../../../utils"
 
+export const urls = {
+  high: getBaseUrl + "/highIntentPath",
+  med: getBaseUrl + "/medIntentPath",
+  low: getBaseUrl + "/lowIntentPath",
+
+  forNow:
+    "https://tru-g-2025.netlify.app/iframe.html?id=pages-landing-test--default&viewMode=story",
+} as const;
+
+//Let's hope a simpler approach will work
+/*
 type Intent = "high" | "med" | "low" | "forNow";
 
 const lpPaths: Record <Intent, string> = {
@@ -19,4 +30,4 @@ const lpPaths: Record <Intent, string> = {
 export function getlpPath(): string {
     const inte = (process.env.INTE as Intent | undefined) || "forNow";
     return lpPaths[inte] || lpPaths.forNow;
-}
+}*/
