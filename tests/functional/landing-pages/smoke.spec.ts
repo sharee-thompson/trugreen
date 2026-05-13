693b8206096534120e66c98b4c4ccc513b156c07
Jessica <jessica.zager@vml.com>
2026-05-13 13:53:36 -0500

    Now that's really it for the smoke test!

--- tests/functional/landing-pages/smoke.spec.ts
+++ tests/functional/landing-pages/smoke.spec.ts
@@ -1,23 +1,16 @@
 import { test, expect } from "@playwright/test";
 import { urls } from "./landing-page-paths";
+import { components, smokeComponents } from "./components";
 
-for (const [name, url] of Object.entries(urls)) {
-  test(`smoke: ${name} should load`, { tag: ['@smoke', '@landing-pages'] }, async ({ page }) => {
-    await page.goto(url);
-    await expect(page).not.toHaveTitle("Error");
-    //Remaining smoke level assertions
-    /*Common components
-    Nav
-    Logo
-Hero with a CTA (any)
-Section "See the TruGreen difference."
-    non-video variant: Card Wrapper 
-        Card - Before / After
-        Card - Before / After
-        Card - Before / After
-    video variant: Video Embed 
-Pre-Footer, at least one CTA
-Footer
-*/
+for (const [pageName, url] of Object.entries(urls)) {
+  test.describe(`smoke: ${pageName}`, { tag: ["@smoke", "@landing-pages"] }, () => {
+    for (const comp of smokeComponents) {
+      test(`has ${comp}`, async ({ page }) => {
+        await page.goto(url);
+        await expect(page.locator(components[comp])).toBeVisible();
+        await expect(page).not.toHaveTitle("Error");
+        //Update if simple responsiveness test found
+      });
+    }
   });
 }
