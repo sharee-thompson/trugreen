cef887a30d82c7396eb12e700f9c68767d232eed
Jessica <jessica.zager@vml.com>
2026-05-13 12:51:15 -0500

    GOT to have tags!

--- tests/functional/landing-pages/med-intent.spec.ts
+++ tests/functional/landing-pages/med-intent.spec.ts
@@ -1,9 +1,9 @@
 import { test, expect } from "@playwright/test";
 import { urls } from "./landing-page-paths";
 
-test.describe("Test Case ID", () => {
-    test("Medium Intent Landing Page Full Test", async ({ page }) => {
-        //Update forNow to med when url available
-        await page.goto(urls.forNow);
-    });
-})
+test.describe("Test Case 80849", { tag: ["@deep", "@landing-pages"] }, () => {
+  test("Medium Intent Landing Page Full Test", async ({ page }) => {
+    //Update forNow to med when url available
+    await page.goto(urls.forNow);
+  });
+});
--- tests/functional/landing-pages/smoke.spec.ts
+++ tests/functional/landing-pages/smoke.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from "@playwright/test";
 import { urls } from "./landing-page-paths";
 
 for (const [name, url] of Object.entries(urls)) {
-  test(`smoke: ${name} should load`, async ({ page }) => {
+  test(`smoke: ${name} should load`, { tag: ['@smoke', '@landing-pages'] }, async ({ page }) => {
     await page.goto(url);
     await expect(page).not.toHaveTitle("Error");
     //Remaining smoke level assertions
