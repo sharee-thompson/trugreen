dfd010708ee741c830f1a097e136ee84bec57ea8
Jessica <jessica.zager@vml.com>
2026-05-13 11:26:59 -0500

    Set up specific url handler

---
+++ tests/functional/landing-pages/config.ts
@@ -0,0 +1,14 @@
+import { getBaseUrl } from "../../../utils"
+const high =  getBaseUrl + '/highIntentPath';
+const med = getBaseUrl + '/medIntentPath';
+const low = getBaseUrl + '/lowIntentPath';
+
+const forNow =
+  "https://tru-g-2025.netlify.app/iframe.html?id=pages-landing-test--default&viewMode=story";
+
+export = [
+    high,
+    med,
+    low,
+    forNow
+]  
