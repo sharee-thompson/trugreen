dd1ac41f69dfa1bc8e882670a6917af8af78f5f7
Jessica <jessica.zager@vml.com>
2026-05-13 12:51:15 -0500

    GOT to have tags!

--- tests/functional/landing-pages/high-intent.spec.ts
+++ tests/functional/landing-pages/high-intent.spec.ts
@@ -1,9 +1,9 @@
 import { test, expect } from "@playwright/test";
 import { urls } from "./landing-page-paths";
 
-test.describe("Test Case ID", () => {
-    test("High Intent Landing Page Full Test", async ({ page }) => {
-        //Update forNow to high when url available
-        await page.goto(urls.forNow);
-    });
-})
+test.describe("Test Case 80847", { tag: ["@deep", "@landing-pages"] }, () => {
+  test("High Intent Landing Page Full Test", async ({ page }) => {
+    //Update forNow to high when url available
+    await page.goto(urls.forNow);
+  });
+});
--- tests/functional/landing-pages/low-intent.spec.ts
+++ tests/functional/landing-pages/low-intent.spec.ts
@@ -1,9 +1,9 @@
 import { test, expect } from "@playwright/test";
 import { urls } from "./landing-page-paths";
 
-test.describe("Test Case ID", () => {
-    test("Low Intent Landing Page Full Test", async ({ page }) => {
-        //Update forNow to low when url available
-        await page.goto(urls.forNow);
-    });
-})
+test.describe("Test Case 80848", { tag: ["@deep", "@landing-pages"] }, () => {
+  test("Low Intent Landing Page Full Test", async ({ page }) => {
+    //Update forNow to low when url available
+    await page.goto(urls.forNow);
+  });
+});
