a163c4fd33a2aae2da0919831019149e15dc40b1
Jessica <jessica.zager@vml.com>
2026-05-13 13:49:46 -0500

    Set up the components storage

---
+++ tests/functional/landing-pages/components.ts
@@ -0,0 +1,68 @@
+//Requirements in Common
+export const components ={
+nav: '',
+headerLogo: '',
+hero: '',
+heroCtaBase: '',
+sectionSeeTheDifference: '',
+nonVideoCardWrapper: '',
+cardBeforeAfter: '',
+videoEmbed: '',
+preFooter: '',
+preFooterCta: '',
+footer: '',
+
+//Requirements by Intent
+//High 80535
+heroCtaToCall: '',
+ribbon: '',
+ribbonCTA: '',
+ribbonGuarantee:'',
+sectionGetQuote: '',
+sectionCtaLeadForm: '',
+//On click, focus moves to
+leadFormInPage: '',
+
+
+//### Medium 80536
+sectionTheClearChoice: '',
+cardWrapper: '',
+cardIcon: '',
+sectionHowItWorks: '',
+cardWrapperListAndtimeline: '',
+cardNumbered: '',
+        /*cardNumbered1
+        cardNumbered2
+        cardNumbered3
+        cardNumbered4
+        OR just use nth-child when classes are the same*/
+
+ribbonLogo: '',
+logoInRibbon: '',
+sectionQuestions: '',
+faqs: '',
+category: '',
+faq: '',
+
+//### Low 80537
+heroCtaToLeadFormModal: '',
+leadFormModal: '',
+ribbonPhoto: '',
+cardWrapperGrid: '',
+} as const;
+
+export const smokeComponents = [
+
+] as const;
+
+export const highComponents = [
+
+] as const;
+
+export const medComponents = [
+
+] as const;
+
+export const lowComponents = [
+    
+] as const;
