c65e9ad194d316277c19c04783427c52a403bdae
Jessica <jessica.zager@vml.com>
2026-05-13 13:20:17 -0500

    Looks like this will work fine! It attempted to hit all four paths, where three failed as expected.

--- tests/functional/landing-pages/landing-page-paths.ts
+++ tests/functional/landing-pages/landing-page-paths.ts
@@ -1,33 +1,12 @@
 import { getBaseUrl } from "../../../utils"
 
 export const urls = {
-  high: getBaseUrl + "/highIntentPath",
+  //To be updated when urls are available.
+
+    /*high: getBaseUrl + "/highIntentPath",
   med: getBaseUrl + "/medIntentPath",
-  low: getBaseUrl + "/lowIntentPath",
+  low: getBaseUrl + "/lowIntentPath",*/
 
   forNow:
     "https://tru-g-2025.netlify.app/iframe.html?id=pages-landing-test--default&viewMode=story",
 } as const;
-
-//Let's hope a simpler approach will work
-/*
-type Intent = "high" | "med" | "low" | "forNow";
-
-const lpPaths: Record <Intent, string> = {
-
-    high: getBaseUrl + '/highIntentPath',
-    med: getBaseUrl + '/medIntentPath',
-    low: getBaseUrl + '/lowIntentPath',
-   
-    forNow:
-     "https://tru-g-2025.netlify.app/iframe.html?id=pages-landing-test--default&viewMode=story"
-
-
-}
-    
-
-
-export function getlpPath(): string {
-    const inte = (process.env.INTE as Intent | undefined) || "forNow";
-    return lpPaths[inte] || lpPaths.forNow;
-}*/
