3ccdf3dc1f16da1f440dc79f13410eefce1c5045
Jessica <jessica.zager@vml.com>
2026-05-27 10:16:13 -0500

    Updated all available components

--- tests/functional/landing-pages/components.ts
+++ tests/functional/landing-pages/components.ts
@@ -3,40 +3,60 @@ export const components = {
   /*To begin, I'm using the Storybook page for reference, but I need to double check the live versions, especially for LEGACY components. JZ
   https://tru-g-2025.netlify.app/iframe.html?id=pages-landing-test--default&viewMode=story
 
-  When a component is commented out, that means while it's development may be done, it's not included in the Landing Page Test Story
+  When a component is missing a value, that means while it's development may be done, it's not included in the Landing Page Test Story
+
+  This is not laid out inteligently, it's broken down by intent twice. Need to double check for duplicates.
   */
+
+  //All?
   nav: ".landingPageHeader_container__JNUbK",
   headerLogo: ".landingPageHeader_logo__jfr9b",
   hero: ".landingPageHero_heroSection__yEE4B",
   heroCtaBase: ".landingPageHero_ctaWrapper__JgtSm",
   sectionSeeTheDifference:
     ".section_section__mbFim section_section--margin-medium__x9HpW section_section--bg-primary__r8no2",
-  nonVideoCardWrapper: "",
-  cardBeforeAfter: "",
+  nonVideoCardWrapper: ".beforeAfterCard_container__re_Y3",
+  cardBeforeAfter: ".beforeAfterCard_card__egO_V",
+  /*
+  cardBeforeAfterOne: ".beforeAfterCard_card__egO_V:nth-child(1)",
+  cardBeforeAfterTwo: ".beforeAfterCard_card__egO_V:nth-child(2)",
+  cardBeforeAfterThree: ".beforeAfterCard_card__egO_V:nth-child(3)",*/
+
   videoEmbed: "",
-  preFooter: "",
-  preFooterCta: "",
-  footer: "",
+  preFooter: ".preFooter_preFooter__8xHzO  footer-panel bg-black",
+  preFooterCta: ".preFooter_content__VQfVw",
+  footer: "footer",
 
   //Requirements by Intent
   //High 80535
-  heroCtaToCall: "",
-  ribbon: "",
-  ribbonCTA: "",
-  ribbonGuarantee: "",
+  heroCtaToCall:
+    ".cta_cta__klq2F typography_button-text__ubQWS typography_body-text-1-base___iCQd typography_body-text-base__7XZyV cta_cta--primary__I9dsN cta_cta__klq2F typography_button-text__ubQWS typography_body-text-1-base___iCQd typography_body-text-base__7XZyV PhoneContact_phoneLink__eOVPz",
+  ribbon: "", //Which one?
+  ctaRibbon: "#cta-ribbon",
+  ctaRibbonCallButton:
+    ".cta_cta__klq2F typography_button-text__ubQWS typography_body-text-1-base___iCQd typography_body-text-base__7XZyV cta_cta--primary__I9dsN cta_cta__klq2F typography_button-text__ubQWS typography_body-text-1-base___iCQd typography_body-text-base__7XZyV PhoneContact_phoneLink__eOVPz ribbon_phoneCta__RguZi",
+  ctaRibbonButton:
+    ".cta_cta__klq2F typography_button-text__ubQWS typography_body-text-1-base___iCQd typography_body-text-base__7XZyV cta_cta--outline__L3bsY cta_cta__klq2F typography_button-text__ubQWS typography_body-text-1-base___iCQd typography_body-text-base__7XZyV",
+  ribbonGuarantee: "#guarantee-ribbon",
+  ribbonGuaranteeIcon:
+    "img.ribbon_content--small__RRZt_ ribbon_content--sideBySide__eRkqt ribbon_gap--small___XQd_",
   sectionGetQuote: "",
   sectionCtaLeadForm: "",
   //On click, focus moves to
   leadFormInPage: "",
 
   //### Medium 80536
-  sectionTheClearChoice: "",
-  cardWrapper: "",
-  cardIcon: "",
+  sectionClearChoice:
+    ".section_section__mbFim section_section--margin-medium__x9HpW section_section--bg-primary__r8no2",
+  iconCardWrapper: ".iconCard_wrapper__JI9kU",
+  iconCard: ".iconCard_card__eSr8D",
+  /*iconCardOne: ".iconCard_card__eSr8D:nth-child(1)",
+  iconCardTwo: ".iconCard_card__eSr8D:nth-child(2)",
+  iconCardThree: ".iconCard_card__eSr8D:nth-child(3)",*/
   sectionHowItWorks:
     ".section_section__mbFim section_section--margin-medium__x9HpW section_section--bg-secondary__BbvW7",
-  cardWrapperListAndtimeline: "",
-  cardNumbered: "",
+  cardWrapperListAndTimeline: ".numberCard_wrapper__pgtnJ",
+  cardNumbered: ".numberCard_card__wlUcJ",
   /*cardNumbered1
         cardNumbered2
         cardNumbered3
@@ -52,7 +72,7 @@ export const components = {
 
   //### Low 80537
   heroCtaToLeadFormModal: "",
-  leadFormModal: "",
+  leadFormModal: "#lead-modal",
   ribbonPhoto: "",
   cardWrapperGrid: "",
 } as const;
