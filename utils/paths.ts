c200302beda1ffbaa98d0c0917b78b3580539b1f
Jessica <jessica.zager@vml.com>
2026-06-09 16:21:58 -0500

    Updated with legacy & new paths for landing pages

--- utils/paths.ts
+++ utils/paths.ts
@@ -1,3 +1,5 @@
+import { getBaseUrl } from "./config";
+
 const paths = [
   "/",
   "/lawn-care-101",
@@ -9,11 +11,17 @@ const paths = [
 export default paths;
 
 export const landingPagePaths = {
-  //To be updated when urls are available.
 
-  /*high: getBaseUrl + "/highIntentPath",
-  med: getBaseUrl + "/medIntentPath",
-  low: getBaseUrl + "/lowIntentPath",*/
+  high: getBaseUrl + "/b/ppc/landing-page",
+  med: getBaseUrl + "/nb/ppc/landing-page",
+  low: getBaseUrl + "/ppc/landing-page",
+  legacyHigh: getBaseUrl + "/b/ppc/trugreen-lawn-care",
+  legacyMedium: getBaseUrl + "/nb/ppc/lawn-care-service",
+  //I included the provided examples, but we can finetune this list
+  legacyLowSocial: getBaseUrl + '/fb/lawn',
+  legacyLowCampaign: getBaseUrl + '/yt',
+  legacyLowDirectMail: getBaseUrl + 'dm/vp/spring1',
+  legacyLowPromo: getBaseUrl + '/fb/lawn-promo',
 
   forNow:
     "https://tru-g-2025.netlify.app/iframe.html?id=pages-landing-test--default&viewMode=story",
