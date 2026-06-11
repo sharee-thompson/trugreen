001accfe0f9ab9cb4485fb496b967fa2cd34c04a
Jessica Zager <jessica.zager@vml.com>
2026-06-11 15:00:46 -0500

    Updated with just the links for the landing pages as I continue working on the main working branch (#9)

--- utils/paths.ts
+++ utils/paths.ts
@@ -1,3 +1,4 @@
+import { getBaseUrl } from "./index";
 const paths = [
   "/",
   "/lawn-care-101",
@@ -7,3 +8,33 @@ const paths = [
 ];
 
 export default paths;
+
+export const landingPagePaths = {
+  //high: getBaseUrl + "/b/ppc/landing-page",
+  high: "https://dev-trugreen.com/b/ppc/landing-page",
+  //medium: getBaseUrl + "/nb/ppc/landing-page",
+  medium: "https://dev-trugreen.com/nb/ppc/landing-page",
+  //low: getBaseUrl + "/ppc/landing-page",
+  low: "https://dev-trugreen.com/ppc/landing-page",
+
+  //Storybook Examples
+  sbHigh:
+    "https://tru-g-2025.netlify.app/iframe.html?id=pages-landing-page-high-intent--default&viewMode=story",
+  sbMedium:
+    "https://tru-g-2025.netlify.app/iframe.html?id=pages-landing-page-mid-intent--default&viewMode=story",
+  sbLow:
+    "https://tru-g-2025.netlify.app/iframe.html?id=pages-landing-page-low-intent--default&viewMode=story",
+
+} as const;
+
+export const legacyLandingPagePaths = {
+
+  legacyHigh: getBaseUrl + "/b/ppc/trugreen-lawn-care",
+  legacyMedium: getBaseUrl + "/nb/ppc/lawn-care-service",
+  //I included the provided examples, but we can finetune this list
+  legacyLowSocial: getBaseUrl + "/fb/lawn",
+  legacyLowCampaign: getBaseUrl + "/yt",
+  legacyLowDirectMail: getBaseUrl + "dm/vp/spring1",
+  legacyLowPromo: getBaseUrl + "/fb/lawn-promo",
+  
+} as const;
