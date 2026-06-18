d4e57241b48c2fe822ba2837b200aff321cd98b3
Jessica <jessica.zager@vml.com>
2026-06-18 11:48:48 -0500

    Updated imports

--- utils/index.ts
+++ utils/index.ts
@@ -1,7 +1,10 @@
 //Did not include axe helper functions so I didn't break anything
 export { getBaseUrl } from "./config";
 export { emulateLazyLoadScroll } from "./helpers";
+export { closeCookieBanner } from "./helpers";
 //Did not export runWhilePageOpen or isClosedPageError since above uses those
 export { emulateLazyLoadScrollV2 } from "./visualAssistance";
 export { forceFonts } from "./visualAssistance";
+export { selectorsToRemove } from "./visualAssistance";
+export { removeElementIfExists } from "./visualAssistance";
 export { getRandomAddress } from "./address";
