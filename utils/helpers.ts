d6fddc08f6f33aed6730641fc6f07697e968b963
Jessica <jessica.zager@vml.com>
2026-06-18 11:48:38 -0500

    Added a native close cookie baner function

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -1,5 +1,17 @@
 import { Page, TestInfo, expect } from "@playwright/test";
 
+export async function closeCookieBanner(page: Page): Promise<void> {
+  const acceptButton = page.getByRole("button", {name: "Accept All Cookies"});
+
+  try {
+    await acceptButton.waitFor({ state: "visible", timeout: 5000});
+    await acceptButton.click();
+    await acceptButton.waitFor({ state: "hidden", timeout: 5000})
+  } catch {
+
+  }
+}
+
 function isClosedPageError(error: unknown): boolean {
   const message = error instanceof Error ? error.message : String(error);
 
