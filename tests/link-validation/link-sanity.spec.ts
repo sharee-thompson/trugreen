b978001e1ac2d87c632bd00249146f761b42c83c
Jessica <jessica.zager@vml.com>
2026-06-23 15:26:42 -0500

    Cleaned up, tests pass

--- tests/link-validation/link-sanity.spec.ts
+++ tests/link-validation/link-sanity.spec.ts
@@ -1,14 +1,18 @@
-import { expect, request, test, type APIRequestContext } from "@playwright/test";
-import { paths } from "../../utils/paths";
+import { expect, test } from "@playwright/test";
+import  paths  from "../../utils/paths";
 import { getBaseUrl } from "../../utils";
 
-test("Sanity Link Validation", async ({ request }) =>{
+test(
+  "Sanity Link Validation",
+  { tag: ["@sanity", "@link-validation"] },
+  async ({ request }) => {
     for (const path of paths) {
-        const url = new URL(getBaseUrl + path).href;
+      const url = new URL(path, getBaseUrl()).href;
 
-        await test.step(`Validate: ${url}`, async () => {
-            const response = await request.get(url, {failOnStatusCode: false});
-            await expect.soft(response, `Broken path: ${url}`).toBeOK();
-        })
+      await test.step(`Validate: ${url}`, async () => {
+        const response = await request.get(url, { failOnStatusCode: false });
+        await expect.soft(response, `Broken path: ${url}`).toBeOK();
+      });
     }
-})
+  },
+);
