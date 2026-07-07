11ca268997ac2ce5c03f3985ed10f6ded32a6429
Jessica <jessica.zager@vml.com>
2026-07-07 15:03:03 -0500

    Assets completed

--- tests/functional/buy-flow/assets/env.ts
+++ tests/functional/buy-flow/assets/env.ts
@@ -1,8 +1,8 @@
 export const credentials = {
   email: "vmlTest@test.com",
   originalAddress: "3500 Cobble St, Nashville TN 37211",
-  optionalAddress: "",
-  creditCard: "",
+  optionalAddress: "5821 Willowbend Blvd, Houston, TX 77096",
+  creditCard: "4111111111111111",
   firstName: "Test",
   lastName: "VML-Testing",
   cardHolderName: "Test VML-Testing",
