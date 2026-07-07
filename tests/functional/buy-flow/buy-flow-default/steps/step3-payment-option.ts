7bc8c0f6af37611f7b053be5d133b34cfb7f4104
Jessica <jessica.zager@vml.com>
2026-07-07 12:10:31 -0500

    Default isn't using a step library

--- tests/functional/buy-flow/buy-flow-default/steps/step3-payment-option.ts
+++
@@ -1,6 +0,0 @@
-import { Page } from "@playwright/test";
-
-export async function step3PaymentOption(page: Page) {
-  await page.getByRole("radio", { name: "Pay Later", exact: true }).click();
-  await page.getByRole("button", { name: "Continue to Payment" }).click();
-}
--- tests/functional/buy-flow/buy-flow-default/steps/step6-review.ts
+++
@@ -1,6 +0,0 @@
-import { Page, expect } from "@playwright/test";
-
-export async function step6ReviewOrder(page: Page) {
-    await page.waitForLoadState("domcontentloaded");
-  await expect(page.getByText("Review Your Order")).toBeVisible();
-}
