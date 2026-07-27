13724307c3c50bb1c0256c4af54ebff5fb7b9595
Sharee Thompson <sharee.thompson@vml.com>
2026-07-27 11:35:24 -0500

    added urls for accessibility scan

--- utils/axe-paths.ts
+++ utils/axe-paths.ts
@@ -1,3 +1,13 @@
-const paths = ["/", "/lawn-care-101", "/customer-support"];
+const paths = [
+  "/",
+  "/lawn-care-101",
+  "/customer-support",
+  "/products-and-services",
+  "/blog/trugreen-lawn-care-services/trugreen-lawn-care-app",
+  "/pay-your-bill",
+  "/b/ppc/landing-page",
+  "/nb/ppc/landing-page",
+  "/ppc/landing-page",
+];
 
 export default paths;
